3c574e5db0baff78b16fad1d5996ab593986f904
TsangTszKin <1076807544@qq.com>
2019-01-13 22:24:09 +0800

    banner

--- src/components/shop/Swiper.vue
+++ src/components/shop/Swiper.vue
@@ -1,10 +1,10 @@
 <template>
   <mt-swipe :auto="4000" style="height:100px;">
-    <mt-swipe-item><img src="../../assets/images/goods.jpg" /></mt-swipe-item>
-    <mt-swipe-item><img src="../../assets/images/goods.jpg" /></mt-swipe-item>
-    <mt-swipe-item><img src="../../assets/images/goods.jpg" /></mt-swipe-item>
-    <mt-swipe-item><img src="../../assets/images/goods.jpg" /></mt-swipe-item>
-    <mt-swipe-item><img src="../../assets/images/goods.jpg" /></mt-swipe-item>
+    <mt-swipe-item><img src="../../assets/images/banner.jpg" /></mt-swipe-item>
+    <mt-swipe-item><img src="../../assets/images/banner.jpg" /></mt-swipe-item>
+    <mt-swipe-item><img src="../../assets/images/banner.jpg" /></mt-swipe-item>
+    <mt-swipe-item><img src="../../assets/images/banner.jpg" /></mt-swipe-item>
+    <mt-swipe-item><img src="../../assets/images/banner.jpg" /></mt-swipe-item>
   </mt-swipe>
 </template>
 
